5e6d699d0a4865725a404ee28d44cb91df87e289
Morella92 <morena.piem@outlook.com>
2023-05-02 15:24:32 +0200

    FEAT milestone 4 completed

---
+++ src/components/Project.vue
@@ -0,0 +1,37 @@
+<script>
+import axios from 'axios'
+
+export default {
+  data(){
+    return{
+        projects: []
+    }
+  },
+  methods:{
+    fetchProjects(){
+        axios.get('http://127.0.0.1:8000/api/projects')
+        .then(res=>{
+            console.log(res)
+        })
+        .catch(err=>{
+            console.log(err)
+        })
+    }
+  },
+  mounted(){
+    this.fetchProjects()
+  },
+}
+</script>
+
+<template>
+
+  <div class="container">
+    <p v-for="project in projects" :key="project.id">{{project.title}}</p>
+  </div>
+  
+</template>
+
+<style scoped>
+
+</style>
